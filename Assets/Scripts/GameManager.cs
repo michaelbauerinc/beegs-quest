7fb17280dc61aceb1a5698017915004bbb95d75f
Michael Bauer <michael@claimfound.com>
2020-10-18 00:05:14 -0400

    added navigation through map, handled displaying info dynamically based on player input from json

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,16 +6,20 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
     private GameData gameData;
+    private int currentNode = 0;
+    private int input = 0;
+    public bool gameEnded = false;
 
     // Start is called before the first frame update
     void Start () {
         LoadGameData ();
-        SetAllGameOutput (gameData.all_game_data[0]);
     }
 
     // Update is called once per frame
     void Update () {
-
+        GetPlayerInput ();
+        CalculateNextNode (currentNode, input);
+        PlayGame (currentNode);
     }
 
     void SetTitle (string new_title) {
@@ -47,7 +51,7 @@ public class GameManager : MonoBehaviour {
         using (StreamReader stream = new StreamReader ("./data.json")) {
             string json = stream.ReadToEnd ();
             gameData = JsonUtility.FromJson<GameData> (json);
-            Debug.Log (gameData.all_game_data[0].title);
+            // Debug.Log (gameData.all_game_data[0].title);
             // var output = JsonUtility.ToJson (gameData.all_game_data);
             // foreach (var x in gameData.all_game_data) {
             //     // Debug.Log (x.title.ToString ());
@@ -55,6 +59,105 @@ public class GameManager : MonoBehaviour {
             // Debug.Log (output);
         }
 
+    }
+
+    void GetPlayerInput () {
+        input = 0;
+        if (Input.GetKeyUp (KeyCode.Alpha1)) {
+            input = 1;
+
+        } else if (Input.GetKeyUp (KeyCode.Alpha2)) {
+            input = 2;
+        }
+
+    }
+
+    void CalculateNextNode (int node, int input) {
+        if (input == 0) {
+            return;
+        }
+        switch (node) {
+            case 0:
+                if (input == 1) {
+                    currentNode = 1;
+                } else if (input == 2) {
+                    currentNode = 2;
+                }
+                break;
+            case 1:
+                if (input == 1) {
+                    currentNode = 3;
+                } else if (input == 2) {
+                    currentNode = 4;
+                }
+                break;
+            case 2:
+                if (input == 1) {
+                    currentNode = 5;
+                } else if (input == 2) {
+                    currentNode = 6;
+                }
+                break;
+            case 3:
+                if (input == 1) {
+                    currentNode = 7;
+                } else if (input == 2) {
+                    currentNode = 4;
+                }
+                break;
+            case 4:
+                if (input == 1) {
+                    currentNode = 7;
+                } else if (input == 2) {
+                    currentNode = 8;
+                }
+                break;
+            case 5:
+                if (input == 1) {
+                    currentNode = 8;
+                } else if (input == 2) {
+                    currentNode = 9;
+                }
+                break;
+            case 6:
+                if (input == 1) {
+                    currentNode = 5;
+                } else if (input == 2) {
+                    currentNode = 9;
+                }
+                break;
+            case 7:
+                if (input == 1) {
+                    currentNode = 10;
+                } else if (input == 2) {
+                    currentNode = 8;
+                }
+                break;
+            case 8:
+                if (input == 1) {
+                    currentNode = 10;
+                } else if (input == 2) {
+                    currentNode = 10;
+                }
+                break;
+            case 9:
+                if (input == 1) {
+                    currentNode = 8;
+                } else if (input == 2) {
+                    currentNode = 10;
+                }
+                break;
+            case 10:
+                gameEnded = true;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    void PlayGame (int currentNode) {
+        SetAllGameOutput (gameData.all_game_data[currentNode]);
     }
 
     [System.Serializable]
